e003f07385c87087e713a47efeb525a15232d3be
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-09-24 12:28:22 -0500

    Search options added

--- php/hub.php
+++ php/hub.php
@@ -1,14 +1,29 @@
 <?php
 // set post fields
 $post = [
-    'findEntidad' => 'Nombre de la entidad',
-    'registrosXPagina' => 500,
+    'findEntidad' => 'LA GUAJIRA - CÁMARA DE COMERCIO DE LA GUAJIRA', // Company name in the correct format
+    'registrosXPagina' => '500', // How many fields you want to see in the results table
+    'entidad' => '244001018', // Company code or ID in their system
+    /* 'estado' => '', // There are many others options, but you don´t need to know about it now.
+    'departamento' => '',
+    'municipio' => '',
+    'tipoProceso' => '',
+    'objeto' => '',
+    'cuantia' => '',
+    'fechaInicial' => '',
+    'fechaFinal' => '',
+    'numeroProceso' => '',
+    'paginaActual' => '',
+    'totalResultados' => '',
+    'paginaObjetivo' => '',
+    'opnegxdep' => '',
+    'desdeFomulario' => '' */
 ];
 
-$ch = curl_init('https://www.contratos.gov.co/consultas/resultadoListadoProcesos.jsp');
+$ch = curl_init('https://www.contratos.gov.co/consultas/resultadosConsulta.do');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 // execute!
 $response = curl_exec($ch);
 
