8dcb5afb22fcd3e804a70c7be3dd8385f63fd63b
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-09-25 18:28:01 -0500

    Integrated de Datatable with de server side data.

--- php/hub-contratos.gov.co.php
+++ php/hub-contratos.gov.co.php
@@ -53,22 +53,18 @@ $data = []; //final array
 
 // Classical for loop to traverse the rows and communes 
 // of the html table
-for ($i = 5; $i < $total_rows; $i++) {
+for ($i = 1; $i < $total_rows; $i++) { // Warning: I'm not using the first data row at index 0, maybe you need to change 1 to 0 in this line
     $temp_row = []; // to store row values in an array temporarily
     $row = $tr->item($i)->getElementsByTagName('td');
-    for ($j = 1; $j < $row->length; $j++) {
+    for ($j = 1; $j < $row->length; $j++) { // Warning: I'm not using the first data column at index 0, maybe you need to change 1 to 0 in this line
         $temp_row[] = $row->item($j)->textContent;
         $temp_row['link'] = $row->item(1)->childNodes->item(1)->attributes[0]->textContent; // Horrible, but it works
+        $temp_row['link'] = str_replace("javascript: popUpSecop(", '', $temp_row['link']);
+        $temp_row['link'] = str_replace(")", '', $temp_row['link']);
     }
     $data[] = $temp_row;
 }
 
-print_r ($temp_row);
-
-//Extract columns names from first row of the table
-/*for ($i=0; $i<$total_columns;$i++) {
-    $columns_names[] = $tr->item(300)[$i]->textContent;
-}*/
-//echo json_encode($dom->saveHtml($tr[0]));
+echo json_encode(array('data'=>$data));
 
 ?>
