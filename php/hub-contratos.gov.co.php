2baf0689018044c8f9ae19a0c25ba6b0e74c2a22
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-09-25 19:03:57 -0500

    Loading spinner and title added.

--- php/hub-contratos.gov.co.php
+++ php/hub-contratos.gov.co.php
@@ -1,7 +1,8 @@
 <?php
 // set post fields
+$url = 'https://www.contratos.gov.co/'; //Example data service. For educative porpuse only.
 $post = [
-    'findEntidad' => 'LA GUAJIRA - CÁMARA DE COMERCIO DE LA GUAJIRA', // Company name in the correct format
+    'findEntidad' => 'LA GUAJIRA - CÁMARA DE COMERCIO DE LA GUAJIRA', // Enter the company or entity name in the correct format
     'entidad' => '244001018', // Company code or ID in their system
     // There are many others options, but you don´t need to know about it now.
     'estado' => '', 
@@ -33,7 +34,7 @@ $post = [
     'ctl00$ContentPlaceHolder1$hidNombreProveedor' => -1
 ];
 
-$ch = curl_init('https://www.contratos.gov.co/consultas/resultadosConsulta.do');
+$ch = curl_init($url . 'consultas/resultadosConsulta.do');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
@@ -59,12 +60,13 @@ for ($i = 1; $i < $total_rows; $i++) { // Warning: I'm not using the first data 
     for ($j = 1; $j < $row->length; $j++) { // Warning: I'm not using the first data column at index 0, maybe you need to change 1 to 0 in this line
         $temp_row[] = $row->item($j)->textContent;
         $temp_row['link'] = $row->item(1)->childNodes->item(1)->attributes[0]->textContent; // Horrible, but it works
-        $temp_row['link'] = str_replace("javascript: popUpSecop(", '', $temp_row['link']);
-        $temp_row['link'] = str_replace(")", '', $temp_row['link']);
+        $temp_row['link'] = str_replace("javascript: popUpSecop('/", '', $temp_row['link']); // I need to clean the link href text
+        $temp_row['link'] = $url . str_replace(")", '', $temp_row['link']); // And add the site url prefix
     }
     $data[] = $temp_row;
 }
 
-echo json_encode(array('data'=>$data));
-
+// Now I can send the data in JSON format, finally ;)
+echo json_encode(array('data'=>$data)); 
+die();
 ?>
