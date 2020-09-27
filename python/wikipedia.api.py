29e4c7a7689e90e2af2162020e9f6440b7551cc5
pedrozopayares <pedrozopayares@gmail.com>
2020-09-26 20:39:12 -0500

    Update wikipedia.api.py

--- python/wikipedia.api.py
+++ python/wikipedia.api.py
@@ -4,16 +4,16 @@ import requests
 import json
 
 # Parameters for Wikipedia API
-urlLanguage = 'spa'
-urlOutput = 'json'
 wordSearch = 'python'
 
-payload = {'q': 'wordSearch', 'limit': '10'}
+payload = {'q': wordSearch, 'limit': '10'}
 
-urlDictionary = 'https://en.wikipedia.org/w/rest.php/v1/search/page?q='
+urlDictionary = 'https://en.wikipedia.org/w/rest.php/v1/search/page'
 
 res = requests.get(urlDictionary, params = payload)
 data = json.loads(res.text)
 
-print(data['pages'][0]['title'])
-print(data['pages'][0]['excerpt'])
+x = randint(0, 9)
+
+print(data['pages'][x]['title'])
+print(data['pages'][x]['excerpt'])
